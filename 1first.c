bfe281b77f1bb65aa5c99b679647973a6f2279fe
krishnavlog <73338231+krishnavlog@users.noreply.github.com>
2020-12-24 08:41:39 +0545

    Add files via upload

---
+++ 1first.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() //start point of c program
+{ //declaring variables by using int in front of every integer variable
+int tom;
+ int error_code;
+ int TOM;
+//'r'-->character constant
+   printf("this is my first program of c and i am learning with harry ");
+   return 0;
+
+}
